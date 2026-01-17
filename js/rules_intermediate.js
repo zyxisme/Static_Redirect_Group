dc0ba94463c1452ddf6dbe23472a600c24abb4e4
二叉树树 <acofork@qq.com>
2026-01-17 18:50:03 +0800

    Add short link: XydKuvr

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2338,5 +2338,9 @@ window.RULES_INTERMEDIATE = {
     "/cWlfOQi": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/XydKuvr": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };

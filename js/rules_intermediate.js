1fd11764aa5b98d6c0422ca82d8a9b2cff7dec5a
二叉树树 <acofork@qq.com>
2026-01-17 18:56:03 +0800

    Add short link: VwnKsei

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2482,5 +2482,9 @@ window.RULES_INTERMEDIATE = {
     "/tARFPX7": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/VwnKsei": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };

f598848538678e50319317a073e07b26597a06dd
二叉树树 <acofork@qq.com>
2026-01-17 18:56:11 +0800

    Add short link: ebmdi9t

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2542,5 +2542,9 @@ window.RULES_INTERMEDIATE = {
     "/vnNjxKI": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/ebmdi9t": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };

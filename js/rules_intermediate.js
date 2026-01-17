efaee3f4fc5a5b1b2fc524d3383bd6a33580975a
二叉树树 <acofork@qq.com>
2026-01-17 19:00:56 +0800

    Add short link: OSvqlFb

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2730,5 +2730,9 @@ window.RULES_INTERMEDIATE = {
     "/jw9bZCd": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/OSvqlFb": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };

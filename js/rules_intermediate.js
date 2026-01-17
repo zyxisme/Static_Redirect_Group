8b57e12dd4f95af32256134a3add076d878d19ec
二叉树树 <acofork@qq.com>
2026-01-17 18:56:14 +0800

    Add short link: R7tYIol

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2562,5 +2562,9 @@ window.RULES_INTERMEDIATE = {
     "/i2D4hoH": {
         "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
         "expired_at": "2026-01-21T17:00:00.000Z"
+    },
+    "/R7tYIol": {
+        "url": "https://developer.mozilla.org/zh-CN/docs/Web/JavaScript",
+        "expired_at": "2026-01-21T17:00:00.000Z"
     }
 };

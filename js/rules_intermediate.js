1cce4b54f775887aa57e9a1c404b149fbc85884e
二叉树树 <acofork@qq.com>
2026-01-21 20:18:15 +0800

    Add short link: 2etq4d2brh

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -7718,5 +7718,9 @@ window.RULES_INTERMEDIATE = {
     "/rns4gl4j": {
         "url": "https://cv.notaone.cn/",
         "expired_at": "2026-01-27T12:18:13.000Z"
+    },
+    "/2etq4d2brh": {
+        "url": "https://cv.notaone.cn/",
+        "expired_at": "2026-01-27T12:18:15.000Z"
     }
 };

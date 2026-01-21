c8455293b9745a148ef4f4a04be994577b7a1cad
二叉树树 <acofork@qq.com>
2026-01-21 20:11:29 +0800

    Add short link: yi1zfubvzy

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -7058,5 +7058,9 @@ window.RULES_INTERMEDIATE = {
     "/0hq3j": {
         "url": "https://cv.notaone.cn/",
         "expired_at": "2026-01-27T12:11:27.000Z"
+    },
+    "/yi1zfubvzy": {
+        "url": "https://cv.notaone.cn/",
+        "expired_at": "2026-01-27T12:11:29.000Z"
     }
 };

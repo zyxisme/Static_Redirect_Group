443ac550026ceb7e7c172d17fdf82f85b182303f
二叉树树 <acofork@qq.com>
2026-01-24 13:22:53 +0800

    Add short link: fdc6786174

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -8090,5 +8090,9 @@ window.RULES_INTERMEDIATE = {
     "/63214cb873": {
         "url": "https://www.bilibili.com/video/av101",
         "expired_at": "2026-01-30T05:22:45.000Z"
+    },
+    "/fdc6786174": {
+        "url": "https://www.bilibili.com/video/av103",
+        "expired_at": "2026-01-30T05:22:51.000Z"
     }
 };

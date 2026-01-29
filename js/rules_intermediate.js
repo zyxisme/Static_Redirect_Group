0c9b028dac4259517b28c8a9d526afe28a42c8d0
zyx <103358586+zyxisme@users.noreply.github.com>
2026-01-29 20:17:31 +0800

    Add short link: failtest

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -2,5 +2,9 @@ window.RULES_INTERMEDIATE = {
     "/ooooooo": {
         "url": "https://bull.lie",
         "expired_at": "2026-01-30T11:50:00.000Z"
+    },
+    "/failtest": {
+        "url": "https://monesy.cn",
+        "expired_at": "2026-01-31T12:23:00.000Z"
     }
 };

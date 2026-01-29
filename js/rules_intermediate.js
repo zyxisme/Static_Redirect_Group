5b1dd703dd145df41dbd00f58f92b5a20f274ec3
zyx <103358586+zyxisme@users.noreply.github.com>
2026-01-29 20:40:46 +0800

    Add short link: nodeseek

--- js/rules_intermediate.js
+++ js/rules_intermediate.js
@@ -10,5 +10,9 @@ window.RULES_INTERMEDIATE = {
     "/gggggg": {
         "url": "https://aa.aa",
         "expired_at": "2026-01-30T12:26:00.000Z"
+    },
+    "/nodeseek": {
+        "url": "https://nodeseek.com",
+        "expired_at": "2026-01-29T17:40:00.000Z"
     }
 };
